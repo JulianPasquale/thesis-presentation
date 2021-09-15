ef6dbf77b9a53ef0aaf101952146253a378abfe2
Julian Pasquale <julianagustin78@gmail.com>
2021-09-15 19:10:51 -0300

    Update Social app link

--- src/components/Demostration.js
+++ src/components/Demostration.js
@@ -11,7 +11,7 @@ export default () => (
         style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
         width="640"
         height="480"
-        src="https://framer.com/embed/Social-app-for-demo--ftun6BXqSf7WBZ962Ww1/SBG1dwWHL?highlights=0"
+        src="https://framer.com/embed/Social-app-for-demo--ftun6BXqSf7WBZ962Ww1/H08WOJABs?highlights=0"
         allowFullScreen
         title="Framer app"
       />
